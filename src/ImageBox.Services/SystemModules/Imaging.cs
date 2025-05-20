2f798ec3efc366b6df1f4cb9b8acc164ac1144e7
calico-crusade <holycardboardquest@gmail.com>
2025-05-20 15:08:31 -0400

    * Adding CLI releases to gha * Adding more imaging methods

--- src/ImageBox.Services/SystemModules/Imaging.cs
+++ src/ImageBox.Services/SystemModules/Imaging.cs
@@ -1,4 +1,6 @@
-﻿namespace ImageBox.Services.SystemModules;
+﻿using ImageBox.Drawing;
+
+namespace ImageBox.Services.SystemModules;
 
 /// <summary>
 /// Providers a set of functions related to image processing
@@ -137,6 +139,104 @@ public class Imaging(IFileResolverService _resolver, LoadedAst _ast) : IScriptIt
     public Image oilPaint(Image image, int levels = 10, int brushSize = 15)
     {
         image.Mutate(x => x.OilPaint(levels, brushSize));
+        return image;
+    }
+
+    /// <summary>
+    /// Pixelates the image by the given pixel size
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="size">The size of the pixels</param>
+    /// <returns>The image</returns>
+    public Image pixelate(Image image, int size)
+    {
+        image.Mutate(x => x.Pixelate(size));
+        return image;
+    }
+
+    /// <summary>
+    /// Applies the polaroid effect to the image
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <returns>The image</returns>
+    public Image polaroid(Image image)
+    {
+        image.Mutate(x => x.Polaroid());
+        return image;
+    }
+
+    /// <summary>
+    /// Resizes the image to the given width and height
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="width">The width of the image</param>
+    /// <param name="height">The height of the image</param>
+    /// <returns>The image</returns>
+    public Image resize(Image image, int width, int height)
+    {
+        image.Mutate(x => x.Resize(width, height));
+        return image;
+    }
+
+    /// <summary>
+    /// Rotates the image by the given angle
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="angle">The angle to rotate the image by</param>
+    /// <returns>The image</returns>
+    public Image rotate(Image image, float angle)
+    {
+        image.Mutate(x => x.Rotate(angle));
+        return image;
+    }
+
+    /// <summary>
+    /// Saturates the image by the given amount
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="amount">The amount</param>
+    /// <returns>The image</returns>
+    public Image saturate(Image image, float amount)
+    {
+        image.Mutate(x => x.Saturate(amount));
+        return image;
+    }
+
+    /// <summary>
+    /// Applies sepia toning to the image by the given amount
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="amount">The amount</param>
+    /// <returns>The image</returns>
+    public Image sepia(Image image, float amount)
+    {
+        image.Mutate(x => x.Sepia(amount));
+        return image;
+    }
+
+    /// <summary>
+    /// Skews the image by the given amount
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="degreesX">The angle in degrees to perform the skew along the x-axis.</param>
+    /// <param name="degreesY">The angle in degrees to perform the skew along the y-axis.</param>
+    /// <returns>The image</returns>
+    public Image skew(Image image, float degreesX, float degreesY)
+    {
+        image.Mutate(x => x.Skew(degreesX, degreesY));
+        return image;
+    }
+
+    /// <summary>
+    /// Applies a radial vignette effect to the given image.
+    /// </summary>
+    /// <param name="image">The image</param>
+    /// <param name="color">The color to apply</param>
+    /// <returns>The image</returns>
+    public Image vignette(Image image, string color)
+    {
+        var c = color.ParseColor();
+        image.Mutate(x => x.Vignette(c));
         return image;
     }
 }
